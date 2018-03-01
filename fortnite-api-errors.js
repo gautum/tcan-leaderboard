56c967c48defe9230f7d59495fae9689b91ec289
Tommy Barnes <thbarnes@microsoft.com>
2018-03-01 00:39:19 -0500

    Error message statics from the fortnite-api package.

---
+++ fortnite-api-errors.js
@@ -0,0 +1,2 @@
+module.exports.STATS_WrongSyntaxError = "Please precise a good platform: ps4/xb1/pc";
+module.exports.STATS_UserNotFoundError = "Impossible to fetch User. User not found on this platform";
